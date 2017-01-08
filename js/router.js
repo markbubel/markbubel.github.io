778fcb394a525b2ce22548207d05f1db9a8f8804
Mark <mark.bubel@gmail.com>
2017-01-07 19:19:15 -0500

    adding more HTML

--- js/router.js
+++ js/router.js
@@ -1,3 +1,18 @@
-function load(filename) {
-    document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${filename}.html"></object>`;
+function load() {
+    const path = getPath();
+    document.getElementById("content").innerHTML = `<object type="text/html" data="partials/${path}.html"></object>`;
+};
+
+function getPath() {
+    return window.location.href.split("#/")[1];
+};
+
+window.onhashchange = function(event) {
+    event.preventDefault();
+    load();
+};
+
+window.onload = function(event) {
+    event.preventDefault();
+    load();
 };
